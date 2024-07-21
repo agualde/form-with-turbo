63aed47797eadb2b3381a8cb143232077f7104b9
Andrés Gualde <andres@foxsoft.co.uk>
2024-07-21 15:35:42 +0200

    alerts not clearing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load', function() {
+  var flashMessages = document.querySelectorAll('.alert');
+  flashMessages.forEach(function(flashMessage) {
+    setTimeout(function() {
+      flashMessage.style.transition = 'opacity 0.5s ease';
+      flashMessage.style.opacity = 0;
+      setTimeout(function() {
+        flashMessage.remove();
+      }, 200);
+    }, 1000);
+  });
+});
