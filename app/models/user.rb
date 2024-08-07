ad60608befe42bd4e1cb7d2b9e8669cffbf9a3e7
Andrés Gualde <andres@foxsoft.co.uk>
2024-08-07 11:22:59 +0200

    further feedback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-
+  validates :name, :email, presence: true, uniqueness: true
   validates :city, presence: true
+
+  validate :fields_without_html_tags
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+
+  validates :telephone_number, presence: true, numericality: { only_integer: true }, length: { minimum: 10, maximum: 15 }
+
+  private
+
+  def fields_without_html_tags
+    %i[name email city telephone_number].each do |field|
+      if send(field).present? && send(field) =~ /<(.|\n)*?>/
+        errors.add(field, "cannot contain HTML tags")
+      end
+    end
+  end
 end
